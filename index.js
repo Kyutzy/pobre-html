3eb463179e70fe1bf10e247ef4536aa0313e584d
cezap <ccz9000@yahoo.com>
2023-03-08 12:43:14 -0300

    git vomit

---
+++ index.js
@@ -0,0 +1,20 @@
+function pesquisar(){
+    let G_TOKEN = 'AIzaSyCRRiF1SKHxBn3uArMsmUE9RLiERrC5ob8'
+    let nome = document.getElementById("texto").value;
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `https://youtube.googleapis.com/youtube/v3/search?q=${nome}&part=snippet&key=${G_TOKEN}`, true);
+    xhr.send();
+
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            let resposta = xhr.response
+            let respostaObjeto = JSON.parse(resposta)
+            console.log(respostaObjeto)
+            let video = respostaObjeto.items[0].id.videoId
+            console.log(video)
+            let url = `https://www.youtube.com/embed/${video}`
+            document.getElementById("videosuper").src = url
+        }
+    }
+}
